cdc234dbace15cc46a9ba6974089e6ee0c7b55e0
Iqbal-Maulana67 <fikri.rid@gmail.com>
2023-02-25 10:44:38 +0700

    fix repo dir and added minggu 3 task

---
+++ minggu3/app/src/main/java/com/example/minggu3/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.minggu3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
+    ListView listView;
+    ArrayAdapter<CharSequence> adapter;
+
+    Intent acara15 = new Intent(MainActivity.this, acara15_activity.class);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        adapter = ArrayAdapter.createFromResource(this, R.array.countries_array, android.R.layout.simple_list_item_1);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.list, menu);
+        return true;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(this, adapter.getItem(position), Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ minggu3/app/src/main/java/com/example/minggu3/acara15_activity.java
@@ -0,0 +1,42 @@
+package com.example.minggu3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class acara15_activity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private MahasiswaAdapter adapter;
+    private ArrayList<Mahasiswa> mahasiswaArrayList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acara15);
+
+        addData();
+
+        recyclerView = findViewById(R.id.recyler_view);
+
+        adapter = new MahasiswaAdapter(mahasiswaArrayList);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(acara15_activity.this);
+
+        recyclerView.setLayoutManager(layoutManager);
+
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    void addData(){
+        mahasiswaArrayList = new ArrayList<>();
+        mahasiswaArrayList.add(new Mahasiswa("Dimas Maulana", "1414370309", "123456789"));
+        mahasiswaArrayList.add(new Mahasiswa("Fadly Yonk", "1214234560", "987654321"));
+        mahasiswaArrayList.add(new Mahasiswa("Ariyandi Nugraha", "1214230345", "987648765"));
+        mahasiswaArrayList.add(new Mahasiswa("Aham Siswana", "1214378098", "098758124"));
+    }
+}
