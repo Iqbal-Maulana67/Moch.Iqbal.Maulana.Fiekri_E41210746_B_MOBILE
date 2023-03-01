e20d39ecbc8be3a4dea9175871fbe1586235332f
Iqbal-Maulana67 <fikri.rid@gmail.com>
2023-03-01 12:50:34 +0700

    Added Minggu 3 & 4

---
+++ minggu4/app/src/main/java/com/example/minggu4/SecondFragment.java
@@ -0,0 +1,32 @@
+package com.example.minggu4;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class SecondFragment extends Fragment {
+    View view;
+    Button SecondButton;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_second, container, false);
+
+        SecondButton = view.findViewById(R.id.secondButton);
+
+        SecondButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Second Fragment", Toast.LENGTH_LONG).show();
+            }
+        });
+        return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+}
