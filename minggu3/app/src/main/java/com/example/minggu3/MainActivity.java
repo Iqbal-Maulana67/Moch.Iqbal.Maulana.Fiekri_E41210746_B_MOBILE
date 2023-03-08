ada5f7eee7a92a683804d734055b7da81188905e
Iqbal-Maulana67 <fikri.rid@gmail.com>
2023-03-08 10:19:15 +0700

    Update

--- minggu3/app/src/main/java/com/example/minggu3/MainActivity.java
+++ minggu3/app/src/main/java/com/example/minggu3/MainActivity.java
@@ -8,34 +8,34 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
-    ListView listView;
-    ArrayAdapter<CharSequence> adapter;
-
-    Intent acara15 = new Intent(MainActivity.this, acara15_activity.class);
-
+public class MainActivity extends AppCompatActivity{
+    Button layout22, layout23;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView = findViewById(R.id.listView);
-        adapter = ArrayAdapter.createFromResource(this, R.array.countries_array, android.R.layout.simple_list_item_1);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(this);
-    }
+        layout22 = findViewById(R.id.btn_acara22);
+        layout23 = findViewById(R.id.btn_acara23);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.list, menu);
-        return true;
-    }
+        layout22.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent acara22layout = new Intent(MainActivity.this, list_negara.class);
+                startActivity(acara22layout);
+            }
+        });
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(this, adapter.getItem(position), Toast.LENGTH_SHORT).show();
+        layout23.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent acara23layout = new Intent(MainActivity.this, acara15_activity.class);
+                startActivity(acara23layout);
+            }
+        });
     }
 }
---
+++ minggu3/app/src/main/java/com/example/minggu3/list_negara.java
@@ -0,0 +1,40 @@
+package com.example.minggu3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class list_negara extends AppCompatActivity implements AdapterView.OnItemClickListener{
+
+    ListView listView;
+    ArrayAdapter<CharSequence> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_negara);
+
+        listView = findViewById(R.id.listView);
+        adapter = ArrayAdapter.createFromResource(this, R.array.countries_array, android.R.layout.simple_list_item_1);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.list, menu);
+        return true;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(this, adapter.getItem(position), Toast.LENGTH_SHORT).show();
+    }
+}
